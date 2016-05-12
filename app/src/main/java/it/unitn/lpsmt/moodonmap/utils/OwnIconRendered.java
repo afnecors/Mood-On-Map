d8a1b97809249a10beedfdc79b30df14c3071bee
Francesco Serra <afnecors@gmail.com>
2016-05-12 17:22:12 +0200

    Ho dimenticato i file

---
+++ app/src/main/java/it/unitn/lpsmt/moodonmap/utils/OwnIconRendered.java
@@ -0,0 +1,26 @@
+package it.unitn.lpsmt.moodonmap.utils;
+
+import android.content.Context;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.maps.android.clustering.ClusterManager;
+import com.google.maps.android.clustering.view.DefaultClusterRenderer;
+
+/**
+ * Created by Mattia on 08/05/2016.
+ */
+public class OwnIconRendered extends DefaultClusterRenderer<Place> {
+
+    public OwnIconRendered(Context context, GoogleMap map, ClusterManager<Place> clusterManager) {
+        super(context, map, clusterManager);
+    }
+
+    @Override
+    protected void onBeforeClusterItemRendered(Place item, MarkerOptions markerOptions) {
+        markerOptions.icon(item.getIcon());
+        markerOptions.snippet(item.getSnippet());
+        markerOptions.title(item.getTitle());
+        super.onBeforeClusterItemRendered(item, markerOptions);
+    }
+}
