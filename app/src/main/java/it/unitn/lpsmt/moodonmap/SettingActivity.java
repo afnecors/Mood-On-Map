de1ed6159e3ef701bbb33029b0d20f3e42f9b080
jackyboy <giacomojack9@gmail.com>
2016-06-06 11:58:05 +0200

    setting UI

--- app/src/main/java/it/unitn/lpsmt/moodonmap/SettingActivity.java
+++ app/src/main/java/it/unitn/lpsmt/moodonmap/SettingActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.widget.ListView;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -17,6 +19,8 @@ public class SettingActivity extends AppCompatActivity {
 
     ListView lv;
     Context context;
+    private SeekBar range = null;
+    public TextView current=null;
 
     ArrayList prgmName;
     public static int [] prgmImages={R.drawable.bored,R.drawable.lol,R.drawable.sad};
@@ -34,5 +38,41 @@ public class SettingActivity extends AppCompatActivity {
 
         lv=(ListView) findViewById(R.id.listView);
         lv.setAdapter(new ListSettingAdapter(this, prgmNameList, prgmImages));
-    }
+
+
+        current= (TextView) findViewById(R.id.curentValue);
+        range = (SeekBar) findViewById(R.id.seekbar);
+        current.setText(String.valueOf(0));
+
+        range.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+
+            int progressChanged=0;
+
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                progressChanged = progress;
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                    current.setText(String.valueOf(progressChanged));
+            }
+        });
+
+        int min=0;
+        int max=200;
+
+        TextView minValue = (TextView) findViewById(R.id.minValue);
+        TextView maxValue = (TextView) findViewById(R.id.maxValue);
+
+        minValue.setText(String.valueOf(min));
+        maxValue.setText(String.valueOf(max));
+
+
+     }
 }
