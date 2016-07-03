d361ca56688e917317b82969d7ae06dfca58ff09
jackyboy <giacomojack9@gmail.com>
2016-07-03 10:15:03 +0200

    Caricamento near e mymarker

--- app/src/main/java/it/unitn/lpsmt/moodonmap/MyMarker2.java
+++ app/src/main/java/it/unitn/lpsmt/moodonmap/MyMarker2.java
@@ -1,11 +1,14 @@
 package it.unitn.lpsmt.moodonmap;
 
 import android.content.Intent;
+import android.location.Location;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
 import android.widget.Toast;
@@ -20,6 +23,7 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -41,6 +45,12 @@ public class MyMarker2 extends AppCompatActivity {
 
     List<HashMap<String, String>> aList = new ArrayList<HashMap<String, String>>();
 
+    ListView androidListView;
+    SimpleAdapter simpleAdapter;
+    LinearLayout layout;
+    String[] from={"listview_image", "listview_message", "listview_address"};;
+    int[] to= {R.id.listview_image, R.id.listview_message, R.id.listview_address};
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,108 +59,139 @@ public class MyMarker2 extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
 
-        listener = new VolleyResponseListener() {
-            @Override
-            public void onResponse(JSONArray response) {
-                JSONObject jo = null;
-                Gson gson = new Gson();
-                Place p = null;
+        simpleAdapter = new SimpleAdapter(getBaseContext(), aList, R.layout.my_marker2_listview, from, to);
+        androidListView = (ListView) findViewById(R.id.list_view);
+        layout = (LinearLayout) findViewById(R.id.progressbar_view_2);
+        androidListView.setAdapter(simpleAdapter);
 
-                if (response.length() == 0) {
-                    Toast.makeText(MyMarker2.this, "Non hai ancora inserito nessun marker!", Toast.LENGTH_LONG).show();
-                }
-
-                // cicla la lista di oggetti json
-                for (int i = 0; i < response.length(); i++) {
-                    try {
-                        jo = response.getJSONObject(i); // ritorna un singolo oggetto json
-                    } catch (JSONException e) {
-                        e.printStackTrace();
-                    }
-
-                    p = gson.fromJson(jo.toString(), Place.class); // genera l'oggetto Java dal json
-                    p.forceImageFromIdEmo(MyMarker2.this); // aggiunge l'immagine
-                    p.forcePosition(); // aggiunge la posizione
-
-                    String address;
-                    if(p.getAddress(MyMarker2.this).length() > 30) {
-                        address = p.getAddress(MyMarker2.this).substring(0, 30);
-                        address = address.concat("...");
-                    }
-                    else{
-                        address = p.getAddress(MyMarker2.this);
-                    }
-
-                    // gestione del messaggio, tipo se è troppo lungo lo taglio
-                    String message;
-                    if(p.getMessage().length() > 28) {
-                        message = p.getMessage().substring(0, 28);
-                        message = message.concat("...");
-                    }
-                    else{
-                        message = p.getMessage();
-                    }
-
-                    int imgID = getResources().getIdentifier(p.getId_emo(), "drawable", getPackageName());
-                    listViewImage.add(imgID);
-                    listViewMessage.add(message);
-                    listViewAddress.add(address);
-
-                    usersLat.add(p.getLatitude());
-                    usersLng.add(p.getLongitude());
-                }
-
-                // Preparo la listview
-                for (int i = 0; i < listViewMessage.size(); i++) {
-                    HashMap<String, String> hm = new HashMap<String, String>();
-                    hm.put("listview_message", listViewMessage.get(i));
-                    hm.put("listview_image", ""+listViewImage.get(i));
-                    hm.put("listview_address", ""+listViewAddress.get(i));
-                    aList.add(hm);
-                }
-
-                String[] from = {"listview_image", "listview_message", "listview_address"};
-                int[] to = {R.id.listview_image, R.id.listview_message, R.id.listview_address};
-
-                final SimpleAdapter simpleAdapter = new SimpleAdapter(getBaseContext(), aList, R.layout.my_marker2_listview, from, to);
-                final ListView androidListView = (ListView) findViewById(R.id.list_view);
-                androidListView.setAdapter(simpleAdapter);
-
-                // Click Listener
-                androidListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-                    @Override
-                    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                        Intent intent = new Intent(getBaseContext(), MainActivity.class);
-
-                        intent.putExtra("selectedLat", usersLat.get(position));
-                        intent.putExtra("selectedLng", usersLng.get(position));
-                        intent.putExtra("activity_id", "NearMarker");   // al titorno alla Main fa la stessa cosa di Near
-                        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-                        startActivity(intent);
-
-                        //Toast.makeText(getBaseContext(), listViewMessage.get(position),Toast.LENGTH_SHORT).show();
-                    }
-                });
-
-
-                // LongClick Listener
-                androidListView.setOnLongClickListener(new View.OnLongClickListener() {
-                    @Override
-                    public boolean onLongClick(View v) {
-
-                        return false;
-                    }
-                });
-            }
-
-            @Override
-            public void onError(String message) {
-                Toast.makeText(MyMarker2.this, "No data available!", Toast.LENGTH_LONG).show();
-            }
-        };
-        getData(listener);
+        new Task().execute();
     }
 
+    //backaground for spinner:
+    class Task extends AsyncTask<String, Integer, Boolean> {
+        @Override
+        protected void onPreExecute() {
+            layout.setVisibility(View.VISIBLE);
+            androidListView.setVisibility(View.GONE);
+            super.onPreExecute();
+        }
+
+        @Override
+        protected void onPostExecute(Boolean result) {
+            layout.setVisibility(View.GONE);
+            androidListView.setVisibility(View.VISIBLE);
+            simpleAdapter.notifyDataSetChanged();
+            super.onPostExecute(result);
+        }
+
+        @Override
+        protected Boolean doInBackground(String... params) {
+            listener = new VolleyResponseListener() {
+                @Override
+                public void onResponse(JSONArray response) {
+                    JSONObject jo = null;
+                    Gson gson = new Gson();
+                    Place p = null;
+
+                    if (response.length() == 0) {
+                        Toast.makeText(MyMarker2.this, "Non hai ancora inserito nessun marker!", Toast.LENGTH_LONG).show();
+                    }
+
+                    // cicla la lista di oggetti json
+                    for (int i = 0; i < response.length(); i++) {
+                        try {
+                            jo = response.getJSONObject(i); // ritorna un singolo oggetto json
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+                        p = gson.fromJson(jo.toString(), Place.class); // genera l'oggetto Java dal json
+                        p.forceImageFromIdEmo(MyMarker2.this); // aggiunge l'immagine
+                        p.forcePosition(); // aggiunge la posizione
+
+                        String address;
+                        if(p.getAddress(MyMarker2.this).length() > 30) {
+                            address = p.getAddress(MyMarker2.this).substring(0, 30);
+                            address = address.concat("...");
+                        }
+                        else{
+                            address = p.getAddress(MyMarker2.this);
+                        }
+
+                        // gestione del messaggio, tipo se è troppo lungo lo taglio
+                        String message;
+                        if(p.getMessage().length() > 28) {
+                            message = p.getMessage().substring(0, 28);
+                            message = message.concat("...");
+                        }
+                        else{
+                            message = p.getMessage();
+                        }
+
+                        int imgID = getResources().getIdentifier(p.getId_emo(), "drawable", getPackageName());
+                        listViewImage.add(imgID);
+                        listViewMessage.add(message);
+                        listViewAddress.add(address);
+
+                        usersLat.add(p.getLatitude());
+                        usersLng.add(p.getLongitude());
+                    }
+
+                    // Preparo la listview
+                    for (int i = 0; i < listViewMessage.size(); i++) {
+                        HashMap<String, String> hm = new HashMap<String, String>();
+                        hm.put("listview_message", listViewMessage.get(i));
+                        hm.put("listview_image", ""+listViewImage.get(i));
+                        hm.put("listview_address", ""+listViewAddress.get(i));
+                        aList.add(hm);
+                    }
+
+
+
+
+
+                    // Click Listener
+                    androidListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                        @Override
+                        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                            Intent intent = new Intent(getBaseContext(), MainActivity.class);
+
+                            intent.putExtra("selectedLat", usersLat.get(position));
+                            intent.putExtra("selectedLng", usersLng.get(position));
+                            intent.putExtra("activity_id", "NearMarker");   // al titorno alla Main fa la stessa cosa di Near
+                            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                            startActivity(intent);
+
+                            //Toast.makeText(getBaseContext(), listViewMessage.get(position),Toast.LENGTH_SHORT).show();
+                        }
+                    });
+
+
+                    // LongClick Listener
+                    androidListView.setOnLongClickListener(new View.OnLongClickListener() {
+                        @Override
+                        public boolean onLongClick(View v) {
+
+                            return false;
+                        }
+                    });
+                }
+
+                @Override
+                public void onError(String message) {
+                    Toast.makeText(MyMarker2.this, "No data available!", Toast.LENGTH_LONG).show();
+                }
+            };
+            getData(listener);
+
+            try {
+                Thread.sleep(3000);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+    }
     protected void getData(final VolleyResponseListener listener){
         Bundle extras = getIntent().getExtras();
         String android_id = extras.getString("android_id");
--- app/src/main/java/it/unitn/lpsmt/moodonmap/NearMarkerActivity.java
+++ app/src/main/java/it/unitn/lpsmt/moodonmap/NearMarkerActivity.java
@@ -5,6 +5,7 @@ import android.graphics.Bitmap;
 import android.graphics.Color;
 import android.graphics.Typeface;
 import android.location.Location;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -68,6 +69,12 @@ public class NearMarkerActivity extends AppCompatActivity {
     //ArrayList<Integer> listViewDistance = new ArrayList<>();
     List<HashMap<String, String>> aList = new ArrayList<HashMap<String, String>>();
 
+    ListView androidListView;
+    SimpleAdapter simpleAdapter;
+    LinearLayout layout;
+    String[] from={"listview_image", "listview_message", "listview_address"};;
+    int[] to= {R.id.listview_image, R.id.listview_message, R.id.listview_address};
+
     String unit;
 
     @Override
@@ -84,147 +91,179 @@ public class NearMarkerActivity extends AppCompatActivity {
             myLng = extras.getDouble("myLng");
         }
 
-        Log.wtf("myLat is", ""+myLat);
-        Log.wtf("myLng is", ""+myLng);
+        Log.wtf("myLat is", "" + myLat);
+        Log.wtf("myLng is", "" + myLng);
 
         // Da myLat e myLng creo un oggetto Location per definire dove si trova l'utente
         myLocation.setLatitude(myLat);
         myLocation.setLongitude(myLng);
 
-        listener = new VolleyResponseListener() {
-            @Override
-            public void onResponse(JSONArray response) {
-                JSONObject jo = null;
-                Gson gson = new Gson();
-                Place p = null;
+        simpleAdapter = new SimpleAdapter(getBaseContext(), aList, R.layout.near_marker_listview, from, to);
+        androidListView = (ListView) findViewById(R.id.list_view);
+        layout = (LinearLayout) findViewById(R.id.progressbar_view);
+        androidListView.setAdapter(simpleAdapter);
 
-                response = sortJsonArray(response);
-
-                // cicla la lista di oggetti json
-                // voglio soli i primi 10 altrimenti response.length()
-
-                int max;
-                if (response.length() < 10) {
-                    max = response.length();
-                } else {
-                    max = 10;
-                }
-
-                for (int i = 0; i < max; i++) {
-                    try {
-                        jo = response.getJSONObject(i); // ritorna un singolo oggetto json
-                    } catch (JSONException e) {
-                        e.printStackTrace();
-                    }
-
-                    p = gson.fromJson(jo.toString(), Place.class); // genera l'oggetto Java dal json
-                    p.forceImageFromIdEmo(NearMarkerActivity.this); // aggiunge l'immagine
-                    p.forcePosition(); // aggiunge la posizione
-
-                    usersLat.add(p.getLatitude());
-                    usersLng.add(p.getLongitude());
-
-                    // uso l'oggetto usersLocation per calcolare le distanze tra me e gli altri
-                    usersLocation.add(new Location(""));
-                    usersLocation.get(i).setLatitude(usersLat.get(i));
-                    usersLocation.get(i).setLongitude(usersLng.get(i));
-                    //distance.add(myLocation.distanceTo(usersLocation.get(i))); // calcola la distanza tra myLocation e usersLocation[i] e la mette in distance
-
-                    DecimalFormat df = new DecimalFormat(); // cose per i km e metri e arrotondamenti
-
-                    if(myLocation.distanceTo(usersLocation.get(i)) > 1000){
-                        df.setMaximumFractionDigits(2);
-                        distance.add(df.format(myLocation.distanceTo(usersLocation.get(i)) / 1000));
-                        unit = "Km";
-                    }
-                    else{
-                        df.setMaximumFractionDigits(0);
-                        distance.add(df.format(myLocation.distanceTo(usersLocation.get(i))));
-                        unit = "Metri";
-                    }
-
-                    Log.wtf("Distanza", i + " - " + distance.get(i).toString());
-
-                    // gestione dell'indirizzo, tipo se è troppo lungo lo taglio
-                    String address;
-                    if(p.getAddress(NearMarkerActivity.this).length() > 27) {
-                        address = p.getAddress(NearMarkerActivity.this).substring(0, 27);
-                        address = address.concat("...");
-                    }
-                    else{
-                        address = p.getAddress(NearMarkerActivity.this);
-                    }
-
-                    // gestione del messaggio, tipo se è troppo lungo lo taglio
-                    String message;
-                    if(p.getMessage().length() > 28) {
-                        message = p.getMessage().substring(0, 28);
-                        message = message.concat("...");
-                    }
-                    else{
-                        message = p.getMessage();
-                    }
-
-                    int imgID = getResources().getIdentifier(p.getId_emo(), "drawable", getPackageName());
-                    listViewImage.add(imgID);
-                    listViewMessage.add(message);
-                    listViewAddress.add(address + " – " + distance.get(i) + " " + unit);
-                    //listViewDistance.add(distance.get(i));
-                }
-
-                //Collections.sort(listViewDistance); // ordino la lista secondo le distanze
-
-                // Preparo la listview
-                for (int i = 0; i < listViewMessage.size(); i++) {
-                    HashMap<String, String> hm = new HashMap<String, String>();
-                    hm.put("listview_message", listViewMessage.get(i));
-                    hm.put("listview_image", "" + listViewImage.get(i));
-                    hm.put("listview_address", "" + listViewAddress.get(i));
-                    //hm.put("listview_distance", "" + listViewDistance.get(i));
-                    aList.add(hm);
-                }
-
-                String[] from = {"listview_image", "listview_message", "listview_address"};
-                int[] to = {R.id.listview_image, R.id.listview_message, R.id.listview_address};
-
-                final SimpleAdapter simpleAdapter = new SimpleAdapter(getBaseContext(), aList, R.layout.near_marker_listview, from, to);
-                final ListView androidListView = (ListView) findViewById(R.id.list_view);
-                androidListView.setAdapter(simpleAdapter);
-
-                // Click Listener
-                androidListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-                    @Override
-                    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                        Intent intent = new Intent(getBaseContext(), MainActivity.class);
-
-                        intent.putExtra("selectedLat", usersLat.get(position));
-                        intent.putExtra("selectedLng", usersLng.get(position));
-                        intent.putExtra("activity_id", "NearMarker");   // al titorno alla Main fa la stessa cosa di Near
-                        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-                        startActivity(intent);
-
-                        //Toast.makeText(getBaseContext(), listViewMessage.get(position),Toast.LENGTH_SHORT).show();
-                    }
-                });
-
-                // LongClick Listener
-                androidListView.setOnLongClickListener(new View.OnLongClickListener() {
-                    @Override
-                    public boolean onLongClick(View v) {
-
-                        return false;
-                    }
-                });
-            }
-
-            @Override
-            public void onError(String message) {
-                Toast.makeText(NearMarkerActivity.this, "No data available!", Toast.LENGTH_LONG).show();
-            }
-        };
-        getData(listener);
+        new Task().execute();
 
     }
+
+    //backaground for spinner:
+    class Task extends AsyncTask<String, Integer, Boolean> {
+        @Override
+        protected void onPreExecute() {
+            layout.setVisibility(View.VISIBLE);
+            androidListView.setVisibility(View.GONE);
+            super.onPreExecute();
+        }
+
+        @Override
+        protected void onPostExecute(Boolean result) {
+            layout.setVisibility(View.GONE);
+            androidListView.setVisibility(View.VISIBLE);
+            simpleAdapter.notifyDataSetChanged();
+            super.onPostExecute(result);
+        }
+
+        @Override
+        protected Boolean doInBackground(String... params) {
+            listener = new VolleyResponseListener() {
+                @Override
+                public void onResponse(JSONArray response) {
+                    JSONObject jo = null;
+                    Gson gson = new Gson();
+                    Place p = null;
+
+                    response = sortJsonArray(response);
+
+                    // cicla la lista di oggetti json
+                    // voglio soli i primi 10 altrimenti response.length()
+
+                    int max;
+                    if (response.length() < 10) {
+                        max = response.length();
+                    } else {
+                        max = 10;
+                    }
+
+                    for (int i = 0; i < max; i++) {
+                        try {
+                            jo = response.getJSONObject(i); // ritorna un singolo oggetto json
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+                        p = gson.fromJson(jo.toString(), Place.class); // genera l'oggetto Java dal json
+                        p.forceImageFromIdEmo(NearMarkerActivity.this); // aggiunge l'immagine
+                        p.forcePosition(); // aggiunge la posizione
+
+                        usersLat.add(p.getLatitude());
+                        usersLng.add(p.getLongitude());
+
+                        // uso l'oggetto usersLocation per calcolare le distanze tra me e gli altri
+                        usersLocation.add(new Location(""));
+                        usersLocation.get(i).setLatitude(usersLat.get(i));
+                        usersLocation.get(i).setLongitude(usersLng.get(i));
+                        //distance.add(myLocation.distanceTo(usersLocation.get(i))); // calcola la distanza tra myLocation e usersLocation[i] e la mette in distance
+
+                        DecimalFormat df = new DecimalFormat(); // cose per i km e metri e arrotondamenti
+
+                        if(myLocation.distanceTo(usersLocation.get(i)) > 1000){
+                            df.setMaximumFractionDigits(2);
+                            distance.add(df.format(myLocation.distanceTo(usersLocation.get(i)) / 1000));
+                            unit = "Km";
+                        }
+                        else{
+                            df.setMaximumFractionDigits(0);
+                            distance.add(df.format(myLocation.distanceTo(usersLocation.get(i))));
+                            unit = "Metri";
+                        }
+
+                        Log.wtf("Distanza", i + " - " + distance.get(i).toString());
+
+                        // gestione dell'indirizzo, tipo se è troppo lungo lo taglio
+                        String address;
+                        if(p.getAddress(NearMarkerActivity.this).length() > 27) {
+                            address = p.getAddress(NearMarkerActivity.this).substring(0, 27);
+                            address = address.concat("...");
+                        }
+                        else{
+                            address = p.getAddress(NearMarkerActivity.this);
+                        }
+
+                        // gestione del messaggio, tipo se è troppo lungo lo taglio
+                        String message;
+                        if(p.getMessage().length() > 28) {
+                            message = p.getMessage().substring(0, 28);
+                            message = message.concat("...");
+                        }
+                        else{
+                            message = p.getMessage();
+                        }
+
+                        int imgID = getResources().getIdentifier(p.getId_emo(), "drawable", getPackageName());
+                        listViewImage.add(imgID);
+                        listViewMessage.add(message);
+                        listViewAddress.add(address + " – " + distance.get(i) + " " + unit);
+                        //listViewDistance.add(distance.get(i));
+                    }
+
+                    //Collections.sort(listViewDistance); // ordino la lista secondo le distanze
+
+                    // Preparo la listview
+                    for (int i = 0; i < listViewMessage.size(); i++) {
+                        HashMap<String, String> hm = new HashMap<String, String>();
+                        hm.put("listview_message", listViewMessage.get(i));
+                        hm.put("listview_image", "" + listViewImage.get(i));
+                        hm.put("listview_address", "" + listViewAddress.get(i));
+                        //hm.put("listview_distance", "" + listViewDistance.get(i));
+                        aList.add(hm);
+                    }
+
+
+
+
+                    // Click Listener
+                    androidListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                        @Override
+                        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                            Intent intent = new Intent(getBaseContext(), MainActivity.class);
+
+                            intent.putExtra("selectedLat", usersLat.get(position));
+                            intent.putExtra("selectedLng", usersLng.get(position));
+                            intent.putExtra("activity_id", "NearMarker");   // al titorno alla Main fa la stessa cosa di Near
+                            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                            startActivity(intent);
+
+                            //Toast.makeText(getBaseContext(), listViewMessage.get(position),Toast.LENGTH_SHORT).show();
+                        }
+                    });
+
+                    // LongClick Listener
+                    androidListView.setOnLongClickListener(new View.OnLongClickListener() {
+                        @Override
+                        public boolean onLongClick(View v) {
+
+                            return false;
+                        }
+                    });
+                }
+
+                @Override
+                public void onError(String message) {
+                    Toast.makeText(NearMarkerActivity.this, "No data available!", Toast.LENGTH_LONG).show();
+                }
+            };
+            getData(listener);
+
+            try {
+                Thread.sleep(3000);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+    }
+
 
     protected void getData(final VolleyResponseListener listener){
         String url = "http://afnecors.altervista.org/android2016/api.php/markers";
